6ec2b67292a5f2549d713e3a33da4f34b8b18f3e
Hap33 <36961317+Hap33@users.noreply.github.com>
2018-09-25 11:09:53 +0200

    Ajout laser OBJ

--- Assets/Scripts/MachineGun.cs
+++ Assets/Scripts/MachineGun.cs
@@ -6,11 +6,14 @@ using UnityEngine;
 public class MachineGun : MonoBehaviour {
 
     public float rayDistance = 10f;
+    public GameObject Laser;
+    
+    private bool Shot = false;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -19,16 +22,25 @@ public class MachineGun : MonoBehaviour {
 
     void FixedUpdate ()
     {
-        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        /*Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit, rayDistance))
-        {
-            Debug.Log(hit.distance);
-            Debug.Log(hit.collider.gameObject.name);
-            if (Input.GetMouseButton(0))
+        {*/
+            /*Debug.Log(hit.distance);
+            Debug.Log(hit.collider.gameObject.name);*/
+            if (Input.GetMouseButton(0) && Shot == false)
             {
-                hit.rigidbody.AddForce((hit.transform.position - transform.position).normalized * 10f, ForceMode.Impulse);
+                //hit.rigidbody.AddForce((hit.transform.position - transform.position).normalized * 10f, ForceMode.Impulse);
+                Instantiate(Laser, gameObject.transform);
+                Shot = true;
+            StartCoroutine(Cooldown());
             }
-        }
+        //}
+    }
+
+    IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Shot = false;
     }
 }
