f5715d5095a66e7d64d70a1da0da84cce0cefae0
Hap33 <36961317+Hap33@users.noreply.github.com>
2018-09-25 14:36:57 +0200

    MAJ Audio

--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -47,6 +47,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
         private float m_NextStep;
         private bool m_Jumping;
         private AudioSource m_AudioSource;
+        public AudioListener Ears;
 
         // Use this for initialization
         private void Start()
@@ -69,6 +70,10 @@ namespace UnityStandardAssets.Characters.FirstPerson
             m_Jumping = false;
             m_AudioSource = GetComponent<AudioSource>();
 			m_MouseLook.Init(transform , m_Camera.transform);
+        }
+        public override void OnStartLocalPlayer()
+        {
+            Ears.enabled = true;
         }
 
 
