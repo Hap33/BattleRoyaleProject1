b4fd3b9641a0598572b93d97c7884975a27becf2
tenshi17 <freaki-deaki@hotmail.fr>
2018-09-28 11:20:46 +0200

    Skybox network accueil

---
+++ Assets/Scripts/Used_Scripts/MEnuManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MEnuManager : MonoBehaviour {
+
+	void On_Clic_Play ()
+    {
+        SceneManager.LoadScene("Accueil_Network");
+    }
+}
